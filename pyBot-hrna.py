69aa65b9d8784fefabe7364700a383c4d00ca62a
Marko Mäkilä <hrna@lakka.kapsi.fi>
2013-07-13 22:50:24 +0300

    Well, that's a start.. righto?

---
+++ pyBot-hrna.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import socket
+
+##config##
+
+nick = "pyHbot"
+host = "b0xi.eu"
+port = 6667
+ident = "pyHbot"
+chan = "#tsunku"
+realname = "pyHbot"
+
+##END of cfg##
+
+pyBot = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+
+pyBot.connect ((host, port))
+print ("connecting to", host, "in port", port)
+pyBot.send(b"NICK pyHbot")
+pyBot.send(b"USER pyHbot b0xi BULLCRAP :pyHbot")	#yhdistää palvelimellllle...
+
+
+while True:
+print ("IM ALIVE")		#mitä tästä eteenpäin?
+
+
+
+
+
+
+
