b9431d02636aec2af36dacbeab4a46fce3b6b3d4
Marko Mäkilä <hrna@lakka.kapsi.fi>
2013-07-14 22:44:35 +0300

    something

--- modules/op.py
+++ modules/op.py
@@ -1,2 +1,5 @@
 def op ( self ):
-	self.send_data( "MODE " + self.msg[2] + " +o " + self.get_nick() )
+	if self.msg[4] == False:
+		self.send_data( "MODE " + self.msg[2] + " +o " + self.get_nick() )
+	else:
+		self.send_data( "MODE " + self.msg[2] + " +o " + self.msg[4] )
