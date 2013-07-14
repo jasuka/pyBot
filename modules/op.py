329f070742192f4601de67398c9624790e03f693
Jaakko Kuusela <shimatta@gmail.com>
2013-07-14 23:18:55 +0300

    Added and fixed oper check

--- modules/op.py
+++ modules/op.py
@@ -1,13 +1,11 @@
 def op ( self ):
 	try:
-		if self.get_nick() in self.config("opers"):
+		if self.get_nick() in self.config["opers"]:
 			self.send_data( "MODE " + self.msg[2] + " +o " + self.msg[4] )
 		else: 
 			self.send_chan("Unauthorized command")
 	except IndexError:
-
-		if self.get_nick() in self.config("opers"):		
+		if self.get_nick() in self.config["opers"]:		
 			self.send_data( "MODE " + self.msg[2] + " +o " + self.get_nick() )
 		else:
 			self.send_chan("Unauhtorized command")
-		
