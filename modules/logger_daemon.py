3244070730990f09fa8f590bff3db27d2d54b812
Marko Mäkilä <maruchan86@gmail.com>
2013-07-17 00:37:05 +0300

    Each channel has now a separated log file

--- modules/logger_daemon.py
+++ modules/logger_daemon.py
@@ -7,15 +7,15 @@ def logger_daemon ( self ):
 	if "353" in self.msg or "366" in self.msg or "412" in self.msg:
 		return
 	else:
+		chan = self.msg[2]
 		brackets = self.config["TimestampBrackets"].split(",")
-		log = "logs/logger.log"
 		usertxt = ""
 
 		for i in range(3, len(self.msg)):
 			usertxt += self.msg[i] +" "
 	try:
-		chan = self.msg[2]
 		if chan[0] == "#":
+			log = "logs/"+chan+".log"
 			logline = brackets[0]+strftime(self.config["timeformat"])+brackets[1] + " " + self.get_nick() + " @ " + chan + " " + usertxt
 
 			with open(log, "a") as log:
