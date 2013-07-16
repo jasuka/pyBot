1d0796918b5a7502d6e7b07fd5e9be84e704e31f
Jaakko Kuusela <shimatta@gmail.com>
2013-07-16 18:26:33 +0300

    bleh

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
 ## Config
 config = {
-			"host":			"2a00:7b80:3019:12::84e6:a83", 
-			"port":			6667, 
-			"nick":			"pyTsunku", 
-			"altnick": 		"pyTunkio", 
-			"realname":		"pyTsunku", 
-			"ident":		"pyTsunku", 
-			"version":		"pyBot version 0.2", 
-			"debug":		"true",
-			"chans":		"#tsunku",
-			"logging":		"true",
-			"timestamp":	"%H:%M:%S",
-			"opers":		"hrna,jaska",
-			"sysmodules":	"logger_daemon,title",
-			"modules":		"clock,op,version,fmi"
+			"host":				"2a00:7b80:3019:12::84e6:a83", 
+			"port":				6667, 
+			"nick":				"pyTsunku", 
+			"altnick": 			"pyTunkio", 
+			"realname":			"pyTsunku", 
+			"ident":			"pyTsunku", 
+			"version":			"pyBot version 0.2", 
+			"debug":			"true",
+			"chans":			"#tsunku",
+			"logging":			"true",
+			"timeformat":		"%H:%M:%S",
+			"TimestampBrackets":"~,~",
+			"opers":			"hrna,jaska",
+			"sysmodules":		"logger_daemon,title",
+			"modules":			"clock,op,version,fmi"
 				
 		  }
