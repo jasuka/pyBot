5736eb32ac3ec2d0abe10b736774df4945956100
Marko Mäkilä <maruchan86@gmail.com>
2013-07-18 13:29:07 +0300

    Error handling and bug fixes, fixed #4

--- modules/stats.py
+++ modules/stats.py
@@ -3,40 +3,42 @@
 import readline
 
 def stats( self ):
-	
-	if len(self.msg) == 4:
-		self.send_chan("Usage: !stats <nick>")
-	else:
-		try:
-			chan = self.msg[2]	#trying if the 2nd parameter is given in stream data, if yes set it as a channel
+	if self.config["logging"] == "true":
+		if len(self.msg) == 4:
+			self.send_chan("Usage: !stats <nick>")
+		else:
+			try:
+				chan = self.msg[2]	#trying if the 2nd parameter is given in stream data, if yes set it as a channel
 
-		except IndexError as msg:
-			print("Error, something is wrong :(\r\n")
+			except IndexError as msg:
+				print("Error, something is wrong :(\r\n")
 
-		logfile = self.config["log-path"]+chan+".log"
+			logfile = self.config["log-path"]+chan+".log"
 
-		try:
-			with open(logfile): pass	#trying if such logfile exists or not
-		except IOError:
-			self.send_chan("I think i have not been on that channel yet")
+			try:
+				with open(logfile): pass	#trying if such logfile exists or not
+			except IOError:
+				self.send_chan("I think i have not been on that channel yet")
 
-		else:	
+			else:	
 			
-			LookingNick = self.msg[4].rstrip("\r\n")
-			counter = 0
-			log = open(logfile, "r")
-			line = log.readlines()
-			log.close()
+				LookingNick = self.msg[4].rstrip("\r\n")
+				counter = 0
+				log = open(logfile, "r")
+				line = log.readlines()
+				log.close()
 
-			for x in line:
-				line2 = x.split(" ")
-				if LookingNick in line2:
-					counter += 1
+				for x in line:
+					line2 = x.split(" ")
+					if LookingNick in line2:
+						counter += 1
 			
-			if counter is not 0:
-				self.send_chan(LookingNick + " has written " + str(counter) + " lines on this channel ("+ chan +")")
-			else:
-				self.send_chan("I don't remember seeing "+ LookingNick +" on this channel before ("+ chan +")")
+				if counter is not 0:
+					self.send_chan(LookingNick + " has written " + str(counter) + " lines on this channel ("+ chan +")")
+				else:
+					self.send_chan("I don't remember seeing "+ LookingNick +" on this channel before ("+ chan +")")
 		
-			return
+				return
+	else:
+		self.send_chan("First enable logging from config to use this module")
 		
