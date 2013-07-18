48c7e4b2c54a51eb98bb1432238e616dc506b75f
Marko Mäkilä <maruchan86@gmail.com>
2013-07-18 23:07:47 +0300

    taking out needless Try lines of code to speed up the process, now version 1

--- modules/logger_daemon.py
+++ modules/logger_daemon.py
@@ -1,4 +1,4 @@
-##Logger daemon version 0.2.1
+##Logger daemon version 1
 
 from time import gmtime, strftime
 import os
@@ -6,21 +6,18 @@ import os
 def logger_daemon ( self ):
 	if os.path.exists(self.config["log-path"]) == True:
 	
-		try:
-			chan = self.msg[2]
-		except IndexError:
-			print("Error, cannot log\r\n")
-		else:
+		if len(self.msg) >= 4:
 
 			if "353" in self.msg or "366" in self.msg or "412" in self.msg:
 				return
 			else:
 				brackets = self.config["TimestampBrackets"].split(",")
 				usertxt = ""
+				chan = self.msg[2]
 
 				for i in range(3, len(self.msg)):
 					usertxt += self.msg[i] +" "
-			try:
+			#try: 	will cleanup these commented lines later, for now it seems these are not needed
 				if chan[0] == "#":
 					log = self.config["log-path"]+chan+".log"
 					logline = brackets[0]+strftime(self.config["timeformat"])+brackets[1] + " " + self.get_nick() + " @ " + chan + " " + usertxt
@@ -28,9 +25,10 @@ def logger_daemon ( self ):
 					with open(log, "a") as log:
 						log.write(logline)
 						log.flush()
-			except TypeError as msg:
-				if self.config["debug"] == "true":
-					print(msg)
+			#except TypeError as msg:
+				#if self.config["debug"] == "true":
+					#print(msg)
+
 	else:
 		print("Cannot find existing folder for logs, creating: "+self.config["log-path"])
 		os.mkdir(self.config["log-path"])
