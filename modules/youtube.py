cc212f93ebff0e45b5214c4c5facdae20396a014
Jaakko Kuusela <shimatta@gmail.com>
2013-07-18 11:39:55 +0300

    use youtube mobile site

--- modules/youtube.py
+++ modules/youtube.py
@@ -14,7 +14,7 @@ def youtube(self):
 				parameters += "{0} ".format(self.msg[x])
 			parameters_url = urllib.parse.quote(parameters)
 			#print("Parameters: {0}".format(parameters_url))
-			url = "http://youtube.com/results?search_query=" + parameters_url
+			url = "http://m.youtube.com/results?search_query=" + parameters_url
 			#print("URL: {0}".format(url))
 			html = syscmd.getHtml(self, url, True )
 			#print("HÖTÖMÖLÖ funkkarilta: {0}\r\n".format(html))
@@ -24,8 +24,15 @@ def youtube(self):
 			urls = ""
 			soup = BeautifulSoup(html)
 			#print("HÖTÖMÖLÖ: {0}".format(soup))
-			for x in soup.findAll("a", {"class" : "yt-uix-tile-link"})[0:2]:
-				urls += "{0}: http://youtube.com{1} | ".format(x.get('title'), x.get('href'))
+			#for x in soup.findAll("a", {"class" : "yt-uix-tile-link"})[0:2]:
+			for x in soup.findAll("a", {"accesskey" : "1"}):
+				href = x.get('href')
+				href = href[-11:]
+				urls += "{1}: http://youtube.com/{0} | ".format(href, x.string.strip())
+			for x in soup.findAll("a", {"accesskey" : "2"}):
+				href = x.get('href')
+				href = href[-11:]
+				urls += "{1}: http://youtube.com/{0} | ".format(href, x.string.strip())
 			if len(urls) > 0:
 				self.send_chan(urls.strip()[:-1])
 			else:
