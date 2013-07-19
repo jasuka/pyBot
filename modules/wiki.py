2d651a2a512fad6508692e11d2baef7e6eec7524
Jaakko Kuusela <shimatta@gmail.com>
2013-07-19 22:30:27 +0300

    Enhancements

--- modules/wiki.py
+++ modules/wiki.py
@@ -29,7 +29,6 @@ def wiki(self):
 				data = soup.findAll("div", {"class" : "mw-search-result-heading"})
 				output = "{0} : http://en.wikipedia.org{1}".format(data[0].a.get('title'), data[0].a.get('href'))
 				output += " | {0} : http://en.wikipedia.org{1}".format(data[1].a.get('title'), data[1].a.get('href'))
-				#print(data[0].
 				self.send_chan(output)
 			else:
 				self.send_chan("There were no results matching the query.")
