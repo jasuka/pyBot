a460b056977dec1b92646c5f7bf6ed0b244bb011
Marko Mäkilä <maruchan86@gmail.com>
2013-07-19 20:58:22 +0300

    some progress maybe

--- modules/logger_daemon.py
+++ modules/logger_daemon.py
@@ -29,16 +29,42 @@ def logger_daemon ( self ):
 
 	else:
 		try:
+			if self.config["debug"] == "true":
+				print("Cannot find existing folder for logs, creating: "+self.config["log-path"])
 			os.mkdir(self.config["log-path"])
 		except Exception as e:
 			if self.config["debug"] == "true":
 				print(e)
-		if self.config["debug"] == "true":
-			print("Cannot find existing folder for logs, creating: "+self.config["log-path"])
+
 		
 	
-	#if os.path.exists(self.config["log-path"]) == True:
-	#	seendb = self.config["log-path"]+"seendb.txt"
-	#	with open(seendb, "w"
-	# Jäi kesken, tuli muuta.
+	if os.path.exists(self.config["log-path"]) == True:
+		seendb = self.config["log-path"]+"seendb.txt"
+		timeformat = strftime(self.config["timeformat"])
+
+		try:
+			if self.get_nick() in open(seendb).read():
+				with open(seendb, "w", encoding="UTF-8") as temp:
+					for line in open(seendb):				
+						str = "{0}:{1}".format(timeformat,self.get_nick())
+						temp.write(re.sub("^{0}:.*$".format(self.get_nick()), str, line))
+					os.remove(seendb)
+					os.rename(self.config["log-path"]+"temp.txt", seendb)
+					temp.close()
+				return(True)
+			## If the nick doesn't exist in the file, append it in there
+			else:
+				with open(seendb, "a", encoding="UTF-8") as file:
+					str = "\r\n{0}:{1}".format(timeformat,self.get_nick())
+					file.write(str)
+					file.close()
+				return(True)
+		except Exception as e:
+			if self.config["debug"] == "true":
+				print(e)
+				print("Creating file")
+				with open(seendb, "a") as db:
+					db.close()
+				
+	 
 
