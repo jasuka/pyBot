c5ec1b66f3e54688f901ae2c0fd2bef2cc6706d9
Jaakko Kuusela <shimatta@gmail.com>
2013-07-19 22:58:14 +0300

    Enhancements

--- modules/wiki.py
+++ modules/wiki.py
@@ -1,37 +1,38 @@
-#ul class mw-search-results -> li -> div -> a  = title and link
-#div searchresult -> span class searchmatch = text
-
 import urllib.parse
 import syscmd
 from bs4 import BeautifulSoup
 
 def wiki(self):
 
-	if len(self.msg) == 4:
-		self.send_chan("Usage: !wiki <search term>")
-	else:
-		try:
+	if len(self.msg) >= 6:
+		lang = self.msg[4].strip()
+		if len(lang) == 2:
 			parameters = ""
 			length = len(self.msg)
-			
-			for x in range (4, length):
+		
+			for x in range (5, length):
 				parameters += "{0} ".format(self.msg[x])
+				
 			parameters_url = urllib.parse.quote(parameters)
-			
-			url = "http://en.m.wikipedia.org/w/index.php?search=" + parameters_url + "&fulltext=Search"
-			html = syscmd.getHtml(self, url, True )
-		except:
-			if self.config["debug"] == "true":
-				print("Someting went wrong getting the html")
-		try:
-			soup = BeautifulSoup(html)
-			if len(soup.findAll("p", {"class" : "mw-search-nonefound"})) == 0:
-				data = soup.findAll("div", {"class" : "mw-search-result-heading"})
-				output = "{0} : http://en.wikipedia.org{1}".format(data[0].a.get('title'), data[0].a.get('href'))
-				output += " | {0} : http://en.wikipedia.org{1}".format(data[1].a.get('title'), data[1].a.get('href'))
-				self.send_chan(output)
-			else:
-				self.send_chan("There were no results matching the query.")
-		except:
-			if self.config["debug"] == "true":
-				print("Parsing the html failed for some reason")
+			url = "http://{0}.m.wikipedia.org/w/index.php?search={1}&fulltext=Search".format(lang,parameters_url)
+			try:
+				html = syscmd.getHtml(self, url, True )
+			except Exception as e:
+				if self.config["debug"] == "true":
+					print(e)
+			try:
+				soup = BeautifulSoup(html)
+				if len(soup.findAll("p", {"class" : "mw-search-nonefound"})) == 0:
+					data = soup.findAll("div", {"class" : "mw-search-result-heading"})
+					output = "{0} : http://{1}.wikipedia.org{2}".format(data[0].a.get('title'), lang, data[0].a.get('href'))
+					output += " | {0} : http://{1}.wikipedia.org{2}".format(data[1].a.get('title'), lang, data[1].a.get('href'))
+					self.send_chan(output)
+				else:
+					self.send_chan("There were no results matching the query.")
+			except:
+				if self.config["debug"] == "true":
+						print("Parsing the html failed for some reason")
+		else:
+			self.send_chan("Usage: !wiki <lang> <search term> - e.g. !wiki en finland")
+	else:
+		self.send_chan("Usage: !wiki <lang> <search term> - e.g. !wiki en finland")
