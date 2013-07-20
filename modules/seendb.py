648a8d4373e23959270a3570173516950cf07078
Jaakko Kuusela <shimatta@gmail.com>
2013-07-20 23:18:25 +0300

    Fixes

--- modules/seendb.py
+++ modules/seendb.py
@@ -1,37 +1,44 @@
 import re, time, os
 def seendb ( self ):
+	
+	irc_codes = ["001", "002", "003", "004", "005", "042", "251", "250", "252", "254", 
+				"255", "265", "266", "375", "372", "376"]
+
 	if os.path.exists(self.config["log-path"]) == True:
-
-
-		try:
-			seendb		 = self.config["log-path"]+"seen.db"
-			temp		 = self.config["log-path"]+"temp.db"
-			timestamp	 = int(time.time())
-			nick 		 = self.get_nick()
-			usertxt 	 = ""
-
-			for i in range(3, len(self.msg)):
-				usertxt += self.msg[i] +" "
-
-			if nick in open(seendb).read():
-				with open(temp, "w", encoding="UTF-8") as tempdb:
-					for line in open(seendb):				
-						str = "{0};{1}".format(nick,timestamp)
-						tempdb.write(re.sub("^{0};.*$".format(nick), str, line))
-						tempdb.flush()
-					os.remove(seendb)
-					os.rename(temp, seendb)
-				return(True)
-			## If the nick doesn't exist in the file, append it in there
+		
+		if len(self.msg) >= 4:
+			if self.msg[1] in irc_codes:
+				return
 			else:
-				with open(seendb, "a", encoding="UTF-8") as file:
-					str = "\r\n{0};{1}".format(nick,timestamp)
-					file.write(str)
-				return(True)
-		except FileNotFoundError:
-			open(seendb, "a").close()
-			if self.config["debug"] == "true":
-				print("Creating file")
-		except Exception as e:
-			if self.config["debug"] == "true":
-				print(e)
+				try:
+					seendb		 = self.config["log-path"]+"seen.db"
+					temp		 = self.config["log-path"]+"temp.db"
+					timestamp	 = int(time.time())
+					nick 		 = self.get_nick()
+					usertxt 	 = ""
+
+					for i in range(3, len(self.msg)):
+						usertxt += self.msg[i] +" "
+
+					if nick in open(seendb).read():
+						with open(temp, "w", encoding="UTF-8") as tempdb:
+							for line in open(seendb):				
+								str = "{0};{1}".format(nick,timestamp)
+								tempdb.write(re.sub("^{0};.*$".format(nick), str, line))
+								tempdb.flush()
+							os.remove(seendb)
+							os.rename(temp, seendb)
+						return(True)
+					## If the nick doesn't exist in the file, append it in there
+					else:
+						with open(seendb, "a", encoding="UTF-8") as file:
+							str = "\r\n{0};{1}".format(nick,timestamp)
+							file.write(str)
+						return(True)
+				except (OSError, IOError):
+					open(seendb, "a").close()
+					if self.config["debug"] == "true":
+						print("Creating file")
+				except Exception as e:
+					if self.config["debug"] == "true":
+						print(e)
