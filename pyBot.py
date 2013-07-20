31849f3ba1257b41efabe6238879b4e8a8f772da
Jaakko Kuusela <shimatta@gmail.com>
2013-07-20 21:35:28 +0300

    Simplified cmd parser

--- pyBot.py
+++ pyBot.py
@@ -228,7 +228,7 @@ class pyBot():
 					print("Logging disabled\r\n")
 						
 			try:
-				cmd = self.msg[3].rstrip("\r\n").lstrip(":")
+				cmd = self.msg[3].lstrip(":").rstrip("\r\n")
 				if cmd[0] == "!":
 					if "!reload" in cmd:
 						self.reload()
