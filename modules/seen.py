b1442717322d4ffe966467ee2dcf2c776ceb1551
Marko Mäkilä <maruchan86@gmail.com>
2013-07-22 11:19:38 +0300

    Removed double checkup

--- modules/seen.py
+++ modules/seen.py
@@ -16,27 +16,26 @@ def seen ( self ):
 				nick 		= self.msg[4].rstrip("\r\n")
 				nick_in_line 	= 0
 				
-				if nick in open(seendb, "r").read():
-					with open(seendb, "r", encoding="UTF-8") as db:
-						for line in db:
-							if re.search("\\b"+nick+":\\b", line, flags=re.IGNORECASE):
-								spl 		= line.split(":")
-								name 		= spl[0]
-								dbtime 		= spl[1].rstrip("\r\n")
-								dbconvert 	= datetime.datetime.fromtimestamp(int(dbtime)).strftime('[%d.%m.%Y] %H:%M:%S')
-								timediff 	= int(time.time()) - int(dbtime)
-								hours 		= datetime.datetime.fromtimestamp(int(timediff)).strftime('%H')
-								minutes 	= datetime.datetime.fromtimestamp(int(timediff)).strftime('%M')
-								seconds 	= datetime.datetime.fromtimestamp(int(timediff)).strftime('%S')
-								nick_in_line	= 1
+				
+				with open(seendb, "r", encoding="UTF-8") as db:
+					for line in db:
+						if re.search("\\b"+nick+":\\b", line, flags=re.IGNORECASE):
+							spl 		= line.split(":")
+							name 		= spl[0]
+							dbtime 		= spl[1].rstrip("\r\n")
+							dbconvert 	= datetime.datetime.fromtimestamp(int(dbtime)).strftime('[%d.%m.%Y] %H:%M:%S')
+							timediff 	= int(time.time()) - int(dbtime)
+							hours 		= datetime.datetime.fromtimestamp(int(timediff)).strftime('%H')
+							minutes 	= datetime.datetime.fromtimestamp(int(timediff)).strftime('%M')
+							seconds 	= datetime.datetime.fromtimestamp(int(timediff)).strftime('%S')
+							nick_in_line	= 1
 							
-					if nick_in_line == 1:
-						self.send_chan(name+" spoke last time: "+dbconvert+
-							" which is exactly "+hours+" hours "+minutes+" minutes "+seconds+" seconds ago.")
-					else:
-						self.send_chan("I have never logged "+nick+" while being on any channel")
+				if nick_in_line == 1:
+					self.send_chan(name+" spoke last time: "+dbconvert+
+						" which is exactly "+hours+" hours "+minutes+" minutes "+seconds+" seconds ago.")
 				else:
 					self.send_chan("I have never logged "+nick+" while being on any channel")
+
 		else:
 			self.send_chan("Usage: !seen <nick>")
 	else:
