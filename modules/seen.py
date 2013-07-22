51ce8c69c78e8bac5963a4233c23907dc4a8df24
Marko Mäkilä <maruchan86@gmail.com>
2013-07-22 12:32:24 +0300

    Latest revision online

--- modules/seen.py
+++ modules/seen.py
@@ -1,4 +1,6 @@
-import time, datetime
+import time
+import datetime
+import dateutil.relativedelta
 from time import gmtime, strftime
 import re
 
@@ -20,19 +22,30 @@ def seen ( self ):
 				with open(seendb, "r", encoding="UTF-8") as db:
 					for line in db:
 						if re.search("\\b"+nick+":\\b", line, flags=re.IGNORECASE):
-							spl 		= line.split(":")
-							name 		= spl[0]
-							dbtime 		= spl[1].rstrip("\r\n")
-							dbconvert 	= datetime.datetime.fromtimestamp(int(dbtime)).strftime('[%d.%m.%Y] %H:%M:%S')
-							timediff 	= int(time.time()) - int(dbtime)
-							hours 		= datetime.datetime.fromtimestamp(int(timediff)).strftime('%H')
-							minutes 	= datetime.datetime.fromtimestamp(int(timediff)).strftime('%M')
-							seconds 	= datetime.datetime.fromtimestamp(int(timediff)).strftime('%S')
-							nick_in_line	= 1
+							spl = line.split(":")
+							name = spl[0]
+							dbtime = spl[1].rstrip("\r\n")
+							dbconvert = datetime.datetime.fromtimestamp(int(dbtime)).strftime('[%d.%m.%Y // %H:%M:%S]')
+							past = datetime.datetime.fromtimestamp(int(dbtime))
+							current = datetime.datetime.fromtimestamp(int(time.time()))
+							diff = dateutil.relativedelta.relativedelta(current, past)
+							if diff.hours == False:
+								hours = ""
+							else:
+								hours = str(diff.hours)+" hours "
+							if diff.minutes == False:
+								minutes = ""
+							else:
+								minutes = str(diff.minutes)+" minutes "
+							if diff.seconds == False:
+								seconds = "0 seconds "
+							else:
+								seconds = str(diff.seconds)+" seconds "
+							nick_in_line = 1
 							
 				if nick_in_line == 1:
 					self.send_chan(name+" spoke last time: "+dbconvert+
-						" which is exactly "+hours+" hours "+minutes+" minutes "+seconds+" seconds ago.")
+						" which is exactly "+hours+minutes+seconds+"ago.")
 				else:
 					self.send_chan("I have never logged "+nick+" while being on any channel")
 
