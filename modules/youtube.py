ca7469e0a7719700e2eb5c07fce1ea0c78796221
Jaakko Kuusela <shimatta@gmail.com>
2013-07-29 23:52:58 +0300

    Cleaned up code

--- modules/youtube.py
+++ modules/youtube.py
@@ -26,13 +26,9 @@ def youtube(self):
 				soup = BeautifulSoup(html, "lxml")
 			except:
 				soup = BeautifulSoup(html, "html5lib")
-			#else:
-			#	soup = BeautifulSoup(html, "html.parser")
-			## Get the first two urls, note to self: optimize the code if you can think how ;)
+			## Get the first result (adjustable)
 			for x in soup.findAll("a", {"class" : "yt-uix-tile-link"})[0:1]:
-				href = x.get('href')
-				href = href[-11:]
-				urls += "{1}: http://youtube.com/watch?v={0} | ".format(href, x.get('title'))
+				urls += "{1}: http://youtube.com/watch?v={0} | ".format(x.get('href')[-11:], x.get('title'))
 			if len(urls) > 0:
 				self.send_chan(urls.strip()[:-1])
 			else:
