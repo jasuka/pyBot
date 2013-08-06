250c73c3be447ff90359f401a3fa55b4cb3d2538
Marko Mäkilä <maruchan86@gmail.com>
2013-08-06 20:19:29 +0300

    Forgot to change this one

--- modulecfg.py
+++ modulecfg.py
@@ -1,6 +1,6 @@
 modulecfg = {
 			#[MODULES]
 			#Modules & System modules, you better not touch system modules
-			"sysmodules":			"syscmd,logger_daemon,title,seendb,modecheck", #Do not touch this line!!
+			"sysmodules":			"syscmd,logger_daemon,title,seendb", #Do not touch this line!!
 			"modules":			"automodes,cmd,clock,op,version,fmi,currency,youtube,stats,wiki,seen,ylilauta,google,gt,geo,git"
 			}
