4da00b5814200565bf4bea595a692eb148bd65c4
Jaakko Kuusela <shimatta@gmail.com>
2013-08-06 18:38:05 +0300

    Notice instead of msg to channel

--- modules/git.py
+++ modules/git.py
@@ -12,12 +12,12 @@ def git(self):
 		if self.config["debug"] == "true":
 			print(stdoutput.decode("utf-8"))
 		if "fatal" in stdoutput.decode("utf-8"):
-			self.send_chan("Git pull failed")
+			self.send_notice("Git pull failed")
 		if "pyBot.py" in stdoutput.decode("utf-8"):
-			self.send_chan("Pull succeeded, core updated, restarting!")
+			self.send_notice("Pull succeeded, core updated, restarting!")
 			self.restart()
 		else:
-			self.send_chan("Pull succeeded, remember to reload the modules")
+			self.send_notice("Pull succeeded, remember to reload the modules")
 	
 	except Exception as e:
 		if self.config["debug"] == "true":
