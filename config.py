b8a358ccf483ea65f78e6aac45f81e428344f36f
Jaakko Kuusela <shimatta@gmail.com>
2013-08-06 16:14:50 +0300

    Initial git support

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ config = {		#[SERVER]			[Modify the right side only]
 			#[MODULES]
 			#Modules & System modules, you better not touch system modules
 			"sysmodules":			"syscmd,logger_daemon,title,seendb,automodes,modecheck", #Do not touch this line!!
-			"modules":			"cmd,clock,op,version,fmi,currency,youtube,stats,wiki,seen,ylilauta,google,gt,geo",
+			"modules":			"cmd,clock,op,version,fmi,currency,youtube,stats,wiki,seen,ylilauta,google,gt,geo,git",
 
 			#[LOGGING DAEMON]
 			#Loggin			
---
+++ modules/git.py
@@ -0,0 +1,24 @@
+import subprocess
+
+def git(self):
+
+	if self.get_host() not in self.config["opers"]:
+		return
+	try:
+		PIPE = subprocess.PIPE
+		process = subprocess.Popen(['git', 'pull'], stdout=PIPE, stderr=PIPE)
+		stdoutput, stderroutput = process.communicate()
+	
+		if self.config["debug"] == "true":
+			print(stdoutput.decode("utf-8"))
+		if "fatal" in stdoutput.decode("utf-8"):
+			self.send_chan("Git pull failed")
+		if "pyBot.py" in stdoutput.decode("utf-8"):
+			self.send_chan("Pull succeeded, core updated, restarting!")
+			self.restart()
+		else:
+			self.send_chan("Pull succeeded, remember to reload the modules")
+	
+	except Exception as e:
+		if self.config["debug"] == "true":
+			print(e)
