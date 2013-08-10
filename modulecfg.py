3a1e8bc520e91e1760f2acf628d44f9f8d2babfc
Jaakko Kuusela <shimatta@gmail.com>
2013-08-10 20:41:04 +0300

    remove

--- modulecfg.py
+++ modulecfg.py
@@ -2,5 +2,5 @@ modulecfg = {
 			#[MODULES]
 			#Modules & System modules, you better not touch system modules
 			"sysmodules":			"syscmd,logger_daemon,title,seendb", #Do not touch this line!!
-			"modules":			"automodes,cmd,clock,op,version,fmi,currency,youtube,stats,wiki,seen,ylilauta,google,gt,geo,git,ubermod"
+			"modules":			"automodes,cmd,clock,op,version,fmi,currency,youtube,stats,wiki,seen,ylilauta,google,gt,geo,git"
 			}
