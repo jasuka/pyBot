d26a67103bbf88028c3e6aada2d42e92e9c2fc46
Jaakko Kuusela <shimatta@gmail.com>
2013-08-13 18:57:07 +0300

    Test

--- modules/git.py
+++ modules/git.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 def git(self):
 
@@ -19,8 +20,14 @@ def git(self):
 		if "pyBot.py" in stdoutput.decode("utf-8"):
 			self.send_chan("Pull succeeded, core updated, restarting!")
 			self.restart()
-		elif "Already up-to-date." not in stdoutput.decode("utf-8"):
-			self.send_chan("Pull succeeded, remember to reload the modules")
+		if "create mode" in stdoutput.decode("utf-8"):
+			mods = re.findall(r"\create mode 100644 modules/(.*.py)", stdoutput.decode("utf-8"))
+			modules = ""
+			for x in mods:
+				modules += "{0} ".format(x[:-3])
+				self.load(x[:-3])	
+		if "pyBot.py" not in stdoutput.decode("utf-8") and "create mode" not in stdoutput.decode("utf-8"):
+			self.send_chan("Pull succeeded, remember to reload the modules!")
 	
 	except Exception as e:
 		if self.config["debug"] == "true":
