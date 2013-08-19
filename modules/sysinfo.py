d6bc44c0a58daa43f68cf4b2794bbdad198bd3f5
Jaakko Kuusela <shimatta@gmail.com>
2013-08-19 22:55:23 +0300

    Finally linux support

--- modules/sysinfo.py
+++ modules/sysinfo.py
@@ -40,26 +40,26 @@ def sysinfo(self):
 			cpu = cpu.split(":")
 			cpu = cpu[1]
 			
-			## MEM
-			process2 = subprocess.Popen(["top -n 1 | grep 'Mem:' | awk '{print $3 \" \" $5}'"], stdout=PIPE, stderr=PIPE, shell=True)
-			mem, stderroutput = process2.communicate()
-			mem = mem.decode("utf-8").strip()
-			mem = mem.split(" ")
-			used_mem = mem[1][:-1]
-			total_mem = mem[0][:-1]
-			used_mem = round(int(used_mem)/1024.0)
-			total_mem = round(int(total_mem)/1024.0)
-			
+			## MEM // Have to use free -m....
+			#process2 = subprocess.Popen(["top -n 1 | grep 'Mem:' | awk '{print $3 \" \" $5}'"], stdout=PIPE, stderr=PIPE, shell=True)
+			#mem, stderroutput = process2.communicate()
+			#mem = mem.decode("utf-8").strip()
+			#mem = mem.split(" ")
+			#used_mem = mem[1][:-1]
+			#total_mem = mem[0][:-1]
+			#used_mem = round(int(used_mem)/1024.0)
+			#total_mem = round(int(total_mem)/1024.0)
+			total_mem = 200
+			free_mem = 100
 			## Uptime
 			process3 = subprocess.Popen(["uptime"], stdout=PIPE, stderr=PIPE)
 			uptime, stderroutput = process3.communicate()
 			uptime = uptime.decode("utf-8").strip()
 			temp = uptime.split(" ")
-			print(temp)
 			uptime = "{0} {1} {2}".format(temp[2], temp[3], temp[5][:-1])
 
 			self.send_chan("OS: {0} <> Python: {1} <> CPU:{2} <> Uptime: {3} <> Mem Usage: {4}/{5} MiB".format(platform.platform(), 
-					platform.python_version(), cpu, uptime, used_mem, total_mem))	
+					platform.python_version(), " ".join(cpu.split()), uptime, used_mem, total_mem))	
 				
 	except Exception as e:
 		if self.config["debug"] == "true":
