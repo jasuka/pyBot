48cbd114f131c38bdebbfb1967cb7acaff23ec24
Jaakko Kuusela <shimatta@gmail.com>
2013-11-06 18:00:12 +0200

    small fixes

--- modules/isup.py
+++ modules/isup.py
@@ -7,10 +7,13 @@ def isup (self):
 	else:
 		try:
 			url = self.msg[4].strip()
+			if "http://" not in url:
+				url = "http://" + url
 			if urllib.request.urlopen("{}".format(url)).getcode() == 200:
 				self.send_chan("The site {0} seems to be up!".format(url))
 			else:
-				self.send_chan("The site {0} seems to down!".format(url))							
+				self.send_chan("The site {0} seems to be down!".format(url))							
 		except Exception as e:
+			self.send_chan("The site {0} seems to be down!".format(url))
 			if self.config["debug"] == "true":
 				print(e)
