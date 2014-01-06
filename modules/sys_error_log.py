2cb74537f031815be711a94c3e5ccfb91405992c
Marko Mäkilä <maruchan86@gmail.com>
2014-01-06 12:33:00 +0200

    something

--- modules/sys_error_log.py
+++ modules/sys_error_log.py
@@ -12,7 +12,7 @@ def log ( self ):
 		brackets = self.config["TimestampBrackets"].split(",") #Looking brackets from the config file
 
 		log = self.config["log-path"]+"error.log"
-		logline = "{0}{1}{2} {3}\r\n".format( #0-2 timestamp with brackets, 3 nick, 4 channel, 5 the message
+		logline = "{0}{1}{2} {3}\r\n".format( ## 0-2 timestamp, 3 error message
 						brackets[0], strftime(self.config["timeformat"]), brackets[1], self.errormsg)
 
 		with open(log, "a") as logi: #Opening the log and appending the latest result
