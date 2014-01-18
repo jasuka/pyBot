2344db6da821373a69858bf5301c76bdb01d6679
Jaakko Kuusela <shimatta@gmail.com>
2014-01-18 15:01:09 +0200

    Fixed a bug...remove unneccesary spaces and line breaks

--- modules/ylilauta.py
+++ modules/ylilauta.py
@@ -23,7 +23,7 @@ def ylilauta(self):
 			data = soup.findAll("span", {"class" : "postsubject"})
 			x = random.randrange(0,len(data))
 			string = "{0}: {1}".format(data[x].a.string, data[x].a.get('href'))
-			self.send_chan(string)
+			self.send_chan(' '.join(string.split()))
 		except Exception as e:
 			self.errormsg = "[ERROR]-[ylilauta] ylilauta()(2) stating: {0}".format(e)
 			sys_error_log.log( self ) ## LOG the error
