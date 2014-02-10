aedd88254d3994b55acc5294b8619bb365605104
Marko Mäkilä <maruchan86@gmail.com>
2014-02-10 20:02:51 +0200

    More details in error messages

--- sysmodules/sysErrorLog.py
+++ sysmodules/sysErrorLog.py
@@ -4,6 +4,7 @@ from time import gmtime, strftime
 import time
 import os
 import re
+from traceback import format_exc
 
 def log ( self ):
 
@@ -12,8 +13,8 @@ def log ( self ):
 		brackets = self.config["TimestampBrackets"].split(",") #Looking brackets from the config file
 
 		log = self.config["log-path"]+"error.log"
-		logline = "{0}{1}{2} {3}\r\n".format( ## 0-2 timestamp, 3 error message
-						brackets[0], strftime(self.config["timeformat"]), brackets[1], self.errormsg)
+		logline = "{0}{1}{2} {3}. {4}\r\n".format( ## 0-2 timestamp, 3 error message
+						brackets[0], strftime(self.config["timeformat"]), brackets[1], self.errormsg, format_exc())
 
 		with open(log, "a") as logi: #Opening the log and appending the latest result
 			logi.write(logline)
