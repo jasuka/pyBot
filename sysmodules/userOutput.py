b09ee381ea797115dbc9345d1ea748194e609ce6
Marko Mäkilä <maruchan86@gmail.com>
2014-02-15 20:25:53 +0200

    Added support for chan modes display #39

--- sysmodules/userOutput.py
+++ sysmodules/userOutput.py
@@ -4,24 +4,26 @@ import time
 
 def show( self ):
 
-	timeStamp = "[{0}]".format(time.strftime("%d.%m.%Y/%H:%M:%S"))
+	timeStamp = "{0}".format(time.strftime("%H:%M"))
 	try:
+		## Checking if MODE is present
+		if self.msg[1] == "MODE":
+			modeFound = True
+		else:
+			modeFound = False
 
-		## parsing events ONLY if there is a channel present
+		## Checking if the channel is present
 		if self.msg[2][0] == "#":
 			chanVerified = True
-			chan = self.msg[2]
+			chan = self.msg[2].rstrip("\r\n")
 		else:
 			chanVerified = False
 
-		if self.msg[1] == "MODE" and chanVerified:
-			chanMode = True
-			print("{0} MODE {1} [{2} {3}] by {5}".format(
-				timeStamp, chan, self.msg[3], self.msg[4], self.get_nick()))
-		else:
-			chanMode = False
+		if modeFound and chanVerified:
+			print("{0} [{1}] MODE ({2} {3}) by {4}".format(
+				timeStamp, chan, self.msg[3], self.msg[4].rstrip("\r\n"), self.get_nick()))
 
-		if chanVerified and not chanMode:
+		if chanVerified and not modeFound:
 			rawText = []
 			for i in range(3, len(self.msg)):
 				rawText.append(self.msg[i].rstrip("\r\n"))
@@ -32,8 +34,9 @@ def show( self ):
 			
 			text = text[1:]
 
-			print("{0} ({1}) {2} >> {3}".format(
+			print("{0} [{1}] <{2}> {3}".format(
 				timeStamp,chan, self.get_nick(), text))
-	
+			
 	except IndexError:
 		pass
+
