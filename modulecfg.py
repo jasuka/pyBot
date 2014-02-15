941c59fdbf0eddcc19f890e0befc884b782b3ca1
Jaakko Kuusela <shimatta@gmail.com>
2014-02-15 18:10:53 +0200

    what the f is this?

--- modulecfg.py
+++ modulecfg.py
@@ -2,6 +2,5 @@ modulecfg = {
 			#[MODULES]
 			#Modules & System modules, you better not touch system modules
 			"sysmodules":			"syscmd,logger_daemon,title,seendb,sysErrorLog,userOutput", #Do not touch this line!!
-			#"modules":				"cmd"
 			"modules":			"automodes,cmd,clock,seen,cobe,op,version,fmi,currency,youtube,stats,wiki,ylilauta,google,gt,geo,git,sysinfo,isup,conv,tell"
 			}
