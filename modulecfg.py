4f012837578dbe61a69d8950a95140ec40c7507d
Jaakko Kuusela <shimatta@gmail.com>
2014-02-21 17:49:23 +0200

    Moved title to modules...it's not really a system module

--- modulecfg.py
+++ modulecfg.py
@@ -1,6 +1,6 @@
 modulecfg = {
 			#[MODULES]
 			#Modules & System modules, you better not touch system modules
-			"sysmodules":			"syscmd,logger_daemon,title,cobe,seendb,sysErrorLog,userOutput", #Do not touch this line!!
-			"modules":				"automodes,clock,fap,seen,op,version,fmi,currency,youtube,stats,wiki,ylilauta,google,gt,geo,git,sysinfo,isup,conv,tell,stack"
+			"sysmodules":			"syscmd,logger_daemon,cobe,seendb,sysErrorLog,userOutput", #Do not touch this line!!
+			"modules":				"automodes,clock,fap,title,seen,op,version,fmi,currency,youtube,stats,wiki,ylilauta,google,gt,geo,git,sysinfo,isup,conv,tell,stack"
 			}
