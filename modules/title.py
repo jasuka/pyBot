9609e001a8e5fe110b08b8948f3f11c7d4d15dc2
Jaakko Kuusela <shimatta@gmail.com>
2014-05-07 15:07:51 +0300

    Check the content-type of url, get html only if it's text/html

--- modules/title.py
+++ modules/title.py
@@ -7,35 +7,31 @@ import sysErrorLog
 ## Gets title for the urls
 def title ( self, url ):
 	url = syscmd.replaceUmlauts(url)
-	## Banned extensions in the urls
-	banned = re.search( ".*\.(jpg$|jpeg$|png$|gif$|pdf$|exe$|zip$|tar$|tar.gz$|tar.bz2$|iso$|txt$|xml$)", url )
-	
-	if not banned:
-		url = url.strip().rstrip(".")
-		## Until I figure out something better...
-		if "t.co" in url.strip():
-			html = syscmd.getHtml( self, url, False )
-		else:
-			html = syscmd.getHtml( self, url, True )
-		if html:
+	url = url.strip().rstrip(".")
+	## Until I figure out something better...
+	if "t.co" in url.strip():
+		html = syscmd.getHtml( self, url, False )
+	else:
+		html = syscmd.getHtml( self, url, True )
+	if html:
+		try:
 			try:
-				try:
-					soup = BeautifulSoup(html, "lxml")
-				except:
-					soup = BeautifulSoup(html, "html.parser")
-				#else:
-				#	soup = BeautifulSoup(html, "html5lib") #broken!!
-				## We only want to parse the title if it has been found
-				if soup.title:
-					title = soup.title.string
-					title = re.sub("\n", "", title)
-					self.send_chan( "~ " + ' '.join(title.split()) ) ##Split words and join them with space
-				else:
-					self.send_chan( "~ Untitled" )
-			except Exception as e:
-				self.errormsg = "[ERROR]-[title] title() stating: {0} ({1})".format(e, url)
-				sysErrorLog.log( self ) ## LOG the error
-				if self.config["debug"]:
-					print("{0}{1}{2}".format(self.color("red"), self.errormsg, self.color("end")))
-		else:
-			return
+				soup = BeautifulSoup(html, "lxml")
+			except:
+				soup = BeautifulSoup(html, "html.parser")
+			#else:
+			#	soup = BeautifulSoup(html, "html5lib") #broken!!
+			## We only want to parse the title if it has been found
+			if soup.title:
+				title = soup.title.string
+				title = re.sub("\n", "", title)
+				self.send_chan( "~ " + ' '.join(title.split()) ) ##Split words and join them with space
+			else:
+				self.send_chan( "~ Untitled" )
+		except Exception as e:
+			self.errormsg = "[ERROR]-[title] title() stating: {0} ({1})".format(e, url)
+			sysErrorLog.log( self ) ## LOG the error
+			if self.config["debug"]:
+				print("{0}{1}{2}".format(self.color("red"), self.errormsg, self.color("end")))
+	else:
+		return
