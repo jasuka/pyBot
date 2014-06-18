0824b5dc457c15a135da27ae2087355f5a7d65a9
Marko Mäkilä <maruchan86@gmail.com>
2014-06-18 19:46:02 +0300

    kolmen suora

--- modules/game.py
+++ modules/game.py
@@ -7,7 +7,7 @@ class Cache:
 def game( self ):
 
 	gameObjects = [0,1,2,3,4]
-	shuffle(gameObjects)
+	shuffle(gameObjects) 
 	spinnerOne = gameObjects[randint(0,4)]
 	shuffle(gameObjects)
 	spinnerTwo = gameObjects[randint(0,4)]
@@ -17,7 +17,11 @@ def game( self ):
 	if (Cache.winningPool == 0):
 		self.send_chan("Kassasi on tyhjä, et voi pelata :(")
 	else:
-		if ((spinnerOne == spinnerTwo) and (spinnerOne == spinnerThree)):
+		if (((spinnerOne + 1) == spinnerTwo) and (spinnerTwo + 1) == spinnerThree):
+			Cache.winningPool += 5
+			self.send_chan("[{0}] [{1}] [{2}], Kolmen suora, kassassa on on nyt {3} dogecoinia".format(spinnerOne,spinnerTwo,spinnerThree,Cache.winningPool))
+
+		elif ((spinnerOne == spinnerTwo) and (spinnerOne == spinnerThree)):
 			Cache.winningPool += 3
 			self.send_chan("[{0}] [{1}] [{2}], Kolme samaa, kassassa on on nyt {3} dogecoinia".format(spinnerOne,spinnerTwo,spinnerThree,Cache.winningPool))
 
@@ -28,3 +32,4 @@ def game( self ):
 		else:
 			Cache.winningPool -= 1
 			self.send_chan("[{0}] [{1}] [{2}], Ei voittoa :( Kassassa on {3} dogecoinia jäljellä".format(spinnerOne,spinnerTwo,spinnerThree,Cache.winningPool))
+
