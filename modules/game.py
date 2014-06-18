287c4d17cc637fa7ae3fb5b83f6d5e63ccd6b785
Marko Mäkilä <maruchan86@gmail.com>
2014-06-18 17:56:54 +0300

    Increased winning possibilities by half

--- modules/game.py
+++ modules/game.py
@@ -6,7 +6,7 @@ class Cache:
 
 def game( self ):
 
-	gameObjects = [0,1,2,3,4,5,6,7,8,9]
+	gameObjects = [0,1,2,3,4]
 	shuffle(gameObjects)
 	spinnerOne = gameObjects[randint(0,9)]
 	shuffle(gameObjects)
