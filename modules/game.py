73fe27fb61ce4b285975da0690626510c9eb5ac1
Marko Mäkilä <maruchan86@gmail.com>
2014-06-23 00:04:03 +0300

    even prettier

--- modules/game.py
+++ modules/game.py
@@ -13,24 +13,37 @@ class slotGame(object):
 
 	def roundResults(self):
 		if (gameCache.winningPool > 0):
-			if (((self.wheelOne + 1) == self.wheelTwo) and (self.wheelTwo + 1) == self.wheelThree):
+			if(
+				((self.wheelOne + 1) == self.wheelTwo) and
+				(self.wheelTwo + 1) == self.wheelThree
+				):
+
 				gameCache.winningPool += 5
 				roundResult = "[{0}] [{1}] [{2}], Kolmen suora, kassassa on on nyt {3} dogecoinia".format(
 					self.wheelOne, self.wheelTwo, self.wheelThree, gameCache.winningPool)
 				return(roundResult)
 
-			elif ((self.wheelOne == self.wheelTwo) and (self.wheelOne == self.wheelThree)):
+			elif(
+				(self.wheelOne == self.wheelTwo) and
+				(self.wheelOne == self.wheelThree)
+				):
+
 				gameCache.winningPool += 3
 				roundResult = "[{0}] [{1}] [{2}], Kolme samaa, kassassa on on nyt {3} dogecoinia".format(
 					self.wheelOne, self.wheelTwo, self.wheelThree, gameCache.winningPool)
 				return(roundResult)
 
-			elif ((self.wheelOne == self.wheelTwo) or (self.wheelTwo == self.wheelThree) or (self.wheelOne == self.wheelThree)):
+			elif(
+				(self.wheelOne == self.wheelTwo) or
+				(self.wheelTwo == self.wheelThree) or
+				(self.wheelOne == self.wheelThree)
+				):
+			
 				gameCache.winningPool += 2
 				roundResult = "[{0}] [{1}] [{2}], Kaksi samaa, kassassa on nyt {3} dogecoinia".format(
 					self.wheelOne, self.wheelTwo, self.wheelThree, gameCache.winningPool)
 				return(roundResult)
-				
+
 			else:
 				gameCache.winningPool -= 1
 				roundResult = "[{0}] [{1}] [{2}], Ei voittoa :( Kassassa on {3} dogecoinia jäljellä".format(
